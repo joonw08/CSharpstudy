765f2356192e529f681030774ba62efeca1491a1
joonw08 <joonwoo8585@gmail.com>
2024-08-10 17:37:56 +0900

    C#첫 프로그램

--- Csharpstudy/Program.cs
+++ Csharpstudy/Program.cs
@@ -5,6 +5,17 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+
+            int num = 10;
+            float f1 = 1.1f;
+            char c1 = 'c';
+            string s1 = "string";
+
+
+            int num = 10;
+
+            Console.WriteLine(num);
+
         }
     }
 }
