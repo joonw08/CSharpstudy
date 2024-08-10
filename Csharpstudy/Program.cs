4073db173a212e066f96e6fc1e9f19656c16df78
joonw08 <joonwoo8585@gmail.com>
2024-08-10 16:00:53 +0900

    첫커밋

---
+++ Csharpstudy/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Csharpstudy
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
